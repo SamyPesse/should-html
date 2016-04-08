f682d15bdff0a2ac0d86e08f779b9f7c262026f8
Samy Pessé <samypesse@gmail.com>
2016-04-08 11:24:17 +0200

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # should-html
-HTML testing assertion for should.js
+
+> HTML testing assertion for should.js.
+
+### Installation
+
+```
+$ npm install should-html --save-dev
+```
+
+### Example
+
+```js
+var should = require('should');
+require('should-html');
+
+it('should return valid html', function() {
+    '<div id="test" class="myDiv">Hello World</div>'.should.be.html('<div class="myDiv" id="test">Hello World</div>');
+});
+```
---
+++ test/test.js
@@ -0,0 +1,14 @@
+var should = require('should');
+require('../');
+
+describe('should-html', function() {
+
+    it('should correctly compare html (equal)', function() {
+        '<div id="test" class="myDiv">Hello World</div>'.should.be.html('<div class="myDiv" id="test">Hello World</div>')
+    });
+
+    it('should correctly compare html (different)', function() {
+        '<div id="test" class="myDiv2">Hello World</div>'.should.not.be.html('<div class="myDiv" id="test">Hello World</div>')
+    });
+
+});
