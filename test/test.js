0088f89e3421187ced368e07a8032f0862a59432
Samy Pesse <samypesse@gmail.com>
2016-05-11 23:08:56 +0200

    Switch to using jsdom and dom-compare

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ describe('should-html', function() {
         '<div id="test" class="myDiv">Hello World</div>'.should.be.html('<div class="myDiv" id="test">Hello World</div>')
     });
 
+    it('should correctly compare html with withespaces (equal)', function() {
+        '<div id="test" class="myDiv">\n\nHello World\n\n</div>'.should.be.html('<div class="myDiv" id="test">Hello World</div>')
+    });
+
     it('should correctly compare html (different)', function() {
         '<div id="test" class="myDiv2">Hello World</div>'.should.not.be.html('<div class="myDiv" id="test">Hello World</div>')
     });
